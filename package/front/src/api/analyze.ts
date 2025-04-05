1cfd50f6e2848fe4117773622eaeafb5f52c275e
taku10101 <taku1011ht@gmai.com>
2025-04-05 20:31:26 +0900

    fix:カロリー計算繋ぎ合わせ

--- package/front/src/api/analyze.ts
+++ package/front/src/api/analyze.ts
@@ -1,9 +1,12 @@
-import { apiPost } from "../utils/apiClient"
 export const getRecognize = async (image:string)  => {
+    const body = new FormData()
+    body.append("image", image)
     try {
-        const data = await apiPost(`http://localhost:8787/api/food/recognize`
-        , image)
-
+        const data = await fetch(`http://localhost:8787/api/food/recognize`,
+            {
+                method: "POST",
+                body: body,
+            })
         return data.json()
     } catch (error) {
         console.error("APIリクエストに失敗しました:", error)
@@ -16,15 +19,10 @@ export const GetAnalyze = async (
     body.append("image", image)
     try {
         const data = await fetch(`http://localhost:8787/api/nutrition/analyze`,
-
             {
                 method: "POST",
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-                body: JSON.stringify(body),
-            }
-            )
+                body: body,
+            })
         return data.json()
     } catch (error) {
         console.error("画像の分析に失敗しました:", error)
